3f56ef0b9206acb46e0b3ee86d426531a93034e1
wahajahmad-cyber <wahajahmad.alnafi@gmail.com>
2025-05-25 16:40:48 +0500

    this is TF infra code

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ips" {
   value = [
     for instance in aws_instance.my_instance : {
-        name        = instance.tags["Name"]
+        name        = instance.tags.Name
         public_ip   = instance.public_ip
 
     }
